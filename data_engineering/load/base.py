3efa54f3bc90295141a51b90a96207dbd9aec016
Jose D. Hernandez-Betancur <jodhernandezbemj@gmail.com>
2021-05-11 23:40:11 -0400

    Version May 10, 2021

--- data_engineering/load/base.py
+++ data_engineering/load/base.py
@@ -5,12 +5,21 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-import os
-
-dir_path = os.path.dirname(os.path.realpath(__file__)) # current directory path
-db_name = 'PRTR_transfers_project.db'
-
-Engine = create_engine(f'sqlite:///{dir_path}/output/{db_name}')
-Session = sessionmaker(bind=Engine)
 
 Base = declarative_base()
+
+def create_engine_session(password,
+                        username='root',
+                        host='127.0.0.1',
+                        port='3306',
+                        db_name='PRTR_transfers_project'):
+    '''
+    Function to create an engine and session in MySQL RDBMS by SQLAlchemy
+    '''
+
+    Engine = create_engine(f'mysql://{username}:{password}@{host}:{port}')
+    Engine.execute(f'CREATE DATABASE IF NOT EXISTS {db_name}')
+    Engine.execute(f'USE {db_name}')
+    Session = sessionmaker(bind=Engine)
+
+    return Engine, Session
--- data_engineering/load/main.py
+++ data_engineering/load/main.py
@@ -10,19 +10,34 @@ import pandas as pd
 import os
 
 from industry_sector import NationalToGenericCode, IsicToGenericCode
-from base import Base, Engine, Session
+from base import Base, create_engine_session
 
 dir_path = os.path.dirname(os.path.realpath(__file__)) # current directory path
 logger = logging.getLogger(__name__)
 
-def main(filename):
-    Base.metadata.create_all(Engine)
-    session = Session()
+def main(args):
+
+    filename = args.filename
+    password = args.password
+    username = args.username
+    host = args.host
+    port = args.port
+    db_name = args.db_name
+    sql_file = args.sql_file
+
+    Engine, Session = create_engine_session(password,
+                            username=username,
+                            host=host,
+                            port=port,
+                            db_name=db_name)
 
     if filename == 'isic_to_generic':
         Object = IsicToGenericCode
     elif filename == 'national_to_generic':
         Object = NationalToGenericCode
+
+    Object.__table__.create(Engine, checkfirst=True)
+    session = Session()
 
     path = f'{dir_path}/../transform/output/{filename}.csv'
     df = pd.read_csv(path)
@@ -36,6 +51,9 @@ def main(filename):
     session.commit()
     session.close()
 
+    if sql_file == 'True':
+        os.system(f'mysqldump -u {username} -p{password} {db_name} > {dir_path}/output/{db_name}.sql')
+
 
 if __name__ == '__main__':
 
@@ -43,7 +61,30 @@ if __name__ == '__main__':
     parser.add_argument('filename',
                         help='The file you want to load into the db',
                         type=str)
+    parser.add_argument('--password',
+                        help='The password for using MySQL RDBMS',
+                        type=str)
+    parser.add_argument('--username',
+                        help='The username for using MySQL RDBMS',
+                        type=str,
+                        default='root')
+    parser.add_argument('--host',
+                        help='The computer hosting for the database',
+                        type=str,
+                        default='127.0.0.1')
+    parser.add_argument('--port',
+                        help='Port used by the database engine',
+                        type=str,
+                        default='3306')
+    parser.add_argument('--db_name',
+                        help='Database name',
+                        type=str,
+                        default='PRTR_transfers_project')
+    parser.add_argument('--sql_file',
+                        help='Would you like to obtain .SQL file',
+                        type=str,
+                        default='False')
 
     args = parser.parse_args()
 
-    main(args.filename)
+    main(args)
