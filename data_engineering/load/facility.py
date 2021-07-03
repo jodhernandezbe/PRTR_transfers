8bff77de6c3d0aebe895b8f3b0ad7812b1fdc75e
Jose D. Hernandez-Betancur <jodhernandezbemj@gmail.com>
2021-07-03 00:54:51 -0400

    Version July 2, 2021

--- data_engineering/load/facility.py
+++ data_engineering/load/facility.py
@@ -6,6 +6,7 @@ from data_engineering.load.base import Base
 
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Facility(Base):
@@ -14,9 +15,16 @@ class Facility(Base):
     national_facility_and_generic_sector_id = Column(Integer(), primary_key=True)
     national_facility_id = Column(String(20), nullable=False)
     national_generic_sector_id = Column(Integer(),
-                                        ForeignKey('national_generic_sector.national_generic_sector_id', ondelete='CASCADE'),
+                                        ForeignKey('national_generic_sector.national_generic_sector_id',
+                                                ondelete='CASCADE',
+                                                onupdate='cascade'),
                                         nullable=False)
     created_at = Column(DateTime(), default=datetime.now())
+
+    national_generic_sector = relationship("NationalGenericSector",
+                                        back_populates="facility")
+    transfer_record = relationship("TransferRecord",
+                                    back_populates="facility")
 
     def __init__(self, **kwargs):
         self.national_facility_and_generic_sector_id = kwargs['national_facility_and_generic_sector_id']
--- data_engineering/load/industry_sector.py
+++ data_engineering/load/industry_sector.py
@@ -6,6 +6,7 @@ from data_engineering.load.base import Base
 
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class NationalGenericSector(Base):
@@ -13,12 +14,23 @@ class NationalGenericSector(Base):
 
     national_generic_sector_id = Column(Integer(), primary_key=True)
     national_sector_id = Column(Integer(),
-                                ForeignKey('national_sector.national_sector_id', ondelete='CASCADE'),
+                                ForeignKey('national_sector.national_sector_id',
+                                        ondelete='CASCADE',
+                                        onupdate='cascade'),
                                 nullable=False)
     generic_sector_code = Column(Integer(),
-                                ForeignKey('generic_sector.generic_sector_code', ondelete='CASCADE'),
+                                ForeignKey('generic_sector.generic_sector_code',
+                                        ondelete='CASCADE',
+                                        onupdate='cascade'),
                                 nullable=False)
     created_at = Column(DateTime(), default=datetime.now())
+
+    national_sector = relationship("NationalSector",
+                                    back_populates="national_generic_sector")
+    generic_sector = relationship("GenericSector",
+                                back_populates="national_generic_sector")
+    facility = relationship("Facility",
+                            back_populates="national_generic_sector")
 
     def __init__(self, **kwargs):
         self.national_generic_sector_id = kwargs['national_generic_sector_id']
@@ -35,6 +47,10 @@ class NationalSector(Base):
     industry_classification_system = Column(String(10), nullable=False)
     created_at = Column(DateTime(), default=datetime.now())
 
+    national_generic_sector = relationship("NationalGenericSector",
+                                        back_populates="national_sector",
+                                        uselist=False)
+
     def __init__(self, **kwargs):
         self.national_sector_id = kwargs['national_sector_id']
         self.national_sector_code = kwargs['national_sector_code']
@@ -48,6 +64,9 @@ class GenericSector(Base):
     generic_sector_code = Column(Integer(), primary_key=True)
     generic_sector_name = Column(String(250), nullable=False)
     created_at = Column(DateTime(), default=datetime.now())
+
+    national_generic_sector = relationship("NationalGenericSector",
+                                        back_populates="generic_sector")
 
     def __init__(self, **kwargs):
         self.generic_sector_code = kwargs['generic_sector_code']
