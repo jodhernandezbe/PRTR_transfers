857e3946476708cdb96044146bfb6c24708a4f2d
jodhernandezbe <jodhernandezbemj@gmail.com>
2022-03-05 13:32:20 +0100

    Version Mar 5, 2022

--- model_deployment/main.py
+++ model_deployment/main.py
@@ -1,4 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Importing libraries
 from fastapi import FastAPI, Query
+from fastapi.responses import JSONResponse, HTTPException
+from fastapi.encoders import jsonable_encoder
+from typing import Optional, List
+
+from model import RequestModel
+from estimations import get_estimations
 
 app = FastAPI(title='PRTR Transfers Model Deployment',
             version='1',
@@ -18,19 +28,56 @@ app = FastAPI(title='PRTR Transfers Model Deployment',
             ''')
 
 
-'''
-Multi-label classification
-'''
+###############################################################################################
+# Multi-label classification
+###############################################################################################
+
 @app.post('/v1/mlc_classification/',
         summary='Multi-label classification predictions',
         tags=['Multi-label classification'])
 async def mlc_classification(
-                prob: bool = Query(False)
+                        input_features: RequestModel,
+                        prob: Optional[bool] = Query(False)
                           ):     
-    return {
-        'mlc_classification': 'mlc_classification'
-    }
 
-'''
-Multi-model binary classification
-'''
+        input_features_dict = input_features.dict()
+
+        estimates = get_estimations(input_features_dict,
+                        prob=prob)
+
+        if not estimates:
+                raise HTTPException(status_code=404,
+                                detail="Descriptors for the input SMILES not found in RDKit.")
+
+        json_compatible_estimates = jsonable_encoder(estimates)
+
+        return JSONResponse(content=json_compatible_estimates)
+
+###############################################################################################
+# Multi-model binary classification
+###############################################################################################
+
+@app.post('/v1/mmbc_classification/',
+        summary='Multi-modlel binary classification (one-vs-all) predictions',
+        tags=['Multi-modlel binary classification (one-vs-all)'])
+async def mlc_classification(
+                        input_features: RequestModel,
+                        prob: Optional[bool] = Query(False),
+                        transfer_class: Optional[List[str]] = Query(['M1', 'M2', 'M3', 'M4', 'M5', 'M6', 'M7', 'M8', 'M9', 'M10'])
+                          ):
+
+        input_features_dict = input_features.dict()
+
+
+        estimates = get_estimations(input_features_dict,
+                        prob=prob,
+                        transfer_class=transfer_class
+                        )
+
+        if not estimates:
+                raise HTTPException(status_code=404,
+                                detail="Descriptors for the input SMILES not found in RDKit.")
+
+        json_compatible_estimates = jsonable_encoder(estimates)
+
+        return JSONResponse(content=json_compatible_estimates)
